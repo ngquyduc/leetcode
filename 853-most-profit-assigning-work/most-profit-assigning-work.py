b33b1336709fafc0c3ec02b223a69379791665d2
Nguyen Quy Duc <quyduc711@gmail.com>
2024-06-18 09:49:12 +0800

    Time: 316 ms (36.43%) | Memory: 19 MB (97.88%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.py
+++ 853-most-profit-assigning-work/most-profit-assigning-work.py
@@ -7,11 +7,12 @@ class Solution:
         for i in range(len(difficulty)):
             if profit[i] > hash_map[difficulty[i]]:
                 hash_map[difficulty[i]] = profit[i]
+        len_dif = len(difficulty)
         difficulty.sort()
         worker.sort()
         highest_profit, lowest_difficulty_ptr, res = 0, 0, 0
         for w in worker:
-            while lowest_difficulty_ptr < len(difficulty) and w >= difficulty[lowest_difficulty_ptr]:
+            while lowest_difficulty_ptr < len_dif and w >= difficulty[lowest_difficulty_ptr]:
                 highest_profit = max(
                     highest_profit, hash_map[difficulty[lowest_difficulty_ptr]]
                 )
